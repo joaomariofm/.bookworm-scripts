1cf59f4f2f1d6f77084bed353c8999e95db80f22
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2024-05-16 14:23:21 -0300

    adjusting fonts download script

--- resources/scripts/nerdfonts.sh
+++ resources/scripts/nerdfonts.sh
@@ -5,23 +5,15 @@ sudo apt install unzip -y
 mkdir -p ~/.local/share/fonts
 
 cd /tmp
-fonts=( 
-"CascadiaCode"
-"FiraCode" 
-"Go-Mono" 
-"Hack"  
-"JetBrainsMono" 
-"Meslo"
-"Mononoki" 
-"RobotoMono" 
-"SourceCodePro" 
-"UbuntuMono"
-)
+fonts=( "Hack" "RobotoMono" )
 
 for font in ${fonts[@]}
 do
 	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/$font.zip
-		unzip $font.zip -d $HOME/.local/share/fonts/$font/
+	unzip $font.zip -d $HOME/.local/share/fonts/
 	rm $font.zip
+	rm $HOME/.local/share/fonts/readme.md
+	rm $HOME/.local/share/fonts/LICENSE.md
+	rm $HOME/.local/share/fonts/LICENSE.txt
 done
 fc-cache
